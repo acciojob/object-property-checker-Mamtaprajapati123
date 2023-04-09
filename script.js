0ae96a689e6386c3b40f911dc86d616c1c724107
Mamtaprajapati123 <124058185+Mamtaprajapati123@users.noreply.github.com>
2023-04-09 18:44:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-var sampleObject = {
-      name: "John",
-      age: 30,
-      city: "New York"
-    };
-    
-    // Define the hasKey function
-    function hasKey(key) {
-      return key in window.sampleObject;
-    }
-    
-    // Test the hasKey function
-    console.log(hasKey("name")); // true
-    console.log(hasKey("address")); // false
+const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+
+function hasKey(key) {
+  //   write your code here
+}
+
+// Do not change the code below
+const key = prompt("Enter Key.");
 alert(hasKey(key));
